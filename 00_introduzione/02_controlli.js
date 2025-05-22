67efa9416d4b7ed58b09a2466b64916619cc990e
Giovanni Pace <g.pace@braintechinnovations.it>
2025-05-22 14:51:10 +0200

    Aggiunta somma

---
+++ 00_introduzione/02_controlli.js
@@ -0,0 +1,11 @@
+//verifica se hai diritto allo sconto giovani
+
+let eta = 19;
+// if(eta > 18)
+//     console.log("Non hai diritto allo sconto");
+// else
+//     console.log("Hai diritto allo sconto");
+
+let risultato = eta > 18 ? "Non hai diritto allo sc." : "Hai diritto allo sc.";
+console.log(risultato)
+
---
+++ 00_introduzione/03_cicli.js
@@ -0,0 +1,15 @@
+// Somma tutti i numeri presenti in un array
+let numeri = [2,5,7,9,10];
+
+// Stampa
+// console.log(numeri);
+// console.log(numeri[2]);
+
+let somma = 0;
+for (let n of numeri){
+    somma = somma + n;
+}
+
+console.log(somma);
+
+//TASK: Fare la media di questo array: let numeri = [2,5,7,9,10];
