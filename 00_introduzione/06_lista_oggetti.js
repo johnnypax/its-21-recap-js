ddfa45b754b1b50424b364340726027f6e9d492d
Giovanni Pace <g.pace@braintechinnovations.it>
2025-05-22 15:33:29 +0200

    Correzione task

--- 00_introduzione/06_lista_oggetti.js
+++ 00_introduzione/06_lista_oggetti.js
@@ -15,3 +15,16 @@ let elenco = [
 
 //Task1: CCercare tutti i libri di JRRT
 //Task2: Contare tutti i libri di JRRT
+
+// for(let l of elenco){
+//     if(l.autore == "JRRT")
+//         console.log(l)
+// }
+
+let contatore = 0;
+for(let l of elenco){
+    if(l.autore == "JRRT")
+        contatore++;
+}
+
+console.log(contatore)
