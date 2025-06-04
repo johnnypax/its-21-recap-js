411614e1e91b6ed0c87ce4c49f5bb8c5735c31c9
Giovanni Pace <g.pace@braintechinnovations.it>
2025-06-04 16:23:14 +0200

    Aggiunta ricerca per Anno

--- 04_task_01/app.js
+++ 04_task_01/app.js
@@ -12,20 +12,41 @@ app.listen(porta, indirizzo, () => {
 })
 
 let biblioteca = [
-    {
-        titolo: "Harry Potter",
-        descrizione: "Boh",
-        isbn: "HPP12345",
-        anno: 1978,
-        autore: "Rowling"
-    },
-    {
-        titolo: "Il signore degli anelli",
-        descrizione: "Boh",
-        isbn: "SGNANL",
-        anno: 1937,
-        autore: "JRRT"
-    }    
+  {
+    "titolo": "Harry Potter e la Pietra Filosofale",
+    "descrizione": "Il primo libro della saga che introduce il giovane orfano Harry Potter al mondo della magia.",
+    "isbn": "978-88-6918-124-0",
+    "anno": 1997,
+    "autore": "J.K. Rowling"
+  },
+  {
+    "titolo": "Il Signore degli Anelli: La Compagnia dell'Anello",
+    "descrizione": "Il primo volume dell'epica trilogia fantasy di Tolkien, che narra le avventure di Frodo Baggins per distruggere l'Anello del Potere.",
+    "isbn": "978-88-452-9261-3",
+    "anno": 1954,
+    "autore": "J.R.R. Tolkien"
+  },
+  {
+    "titolo": "1984",
+    "descrizione": "Un romanzo distopico che esplora i pericoli del totalitarismo e della sorveglianza governativa.",
+    "isbn": "978-88-04-68875-9",
+    "anno": 1997,
+    "autore": "George Orwell"
+  },
+  {
+    "titolo": "Orgoglio e Pregiudizio",
+    "descrizione": "Un classico della letteratura inglese che esplora le dinamiche sociali e le relazioni amorose nell'Inghilterra del XIX secolo.",
+    "isbn": "978-88-04-58962-9",
+    "anno": 1813,
+    "autore": "Jane Austen"
+  },
+  {
+    "titolo": "Cronache di Zylos: L'Ascesa dell'Ombra",
+    "descrizione": "In un mondo sull'orlo della guerra, un giovane eroe scopre un'antica profezia che lo lega al destino di Zylos.",
+    "isbn": "978-88-9999-001-5",
+    "anno": 2023,
+    "autore": "Elara Vance"
+  }
 ]
 
 //Punto 1
@@ -56,3 +77,14 @@ app.get("/libri/:isbn", (req, res) => {
 
 //     res.status(404).json()
 // })
+
+app.get("/libri/anno/:annoRicerca", (req, res) => {
+    let anno_ric = req.params.annoRicerca
+    let risultato = [];
+
+    for(let [indice, libro] of biblioteca.entries())
+        if(libro.anno == anno_ric)
+            risultato.push(libro)
+
+    res.json(risultato)
+})
