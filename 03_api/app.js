8c7c579b1f22a3a1fcd58f8f3d677e145bead7a1
Giovanni Pace <g.pace@braintechinnovations.it>
2025-06-04 14:17:58 +0200

    Aggiunti dati

--- 03_api/app.js
+++ 03_api/app.js
@@ -5,10 +5,26 @@ const app = express()
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-
 const indirizzo = "127.0.0.1";
 const porta = 4000;
 
 app.listen(porta, indirizzo, () => {
     console.log("Sono in ascolto")
+})
+
+let elenco = [
+    {
+        nome: "Giovanni",
+        cognome: "Pace",
+        eta: 38
+    },
+    {
+        nome: "Valeria",
+        cognome: "Verdi",
+        eta: 25
+    },
+];
+
+app.get("/persone", (req, res) => {
+    res.json(elenco)
 })
